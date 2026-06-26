c97e8e9cbe1c2048dce4885079e234a17e0daf10
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-06-27 03:10:06 +0530

    Bulk sync: 62 submissions

---
+++ SQL/sql-insert-default-values/submission-0.sql
@@ -0,0 +1,18 @@
+CREATE TABLE products (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  stock INTEGER DEFAULT 0
+);
+INSERT INTO products (id, name, stock)
+VALUES (1, 'Apple', 0), (2, 'Banana',0), (3, 'Orange',0);
+-- Do not modify above this line --
+
+
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT * FROM products;
